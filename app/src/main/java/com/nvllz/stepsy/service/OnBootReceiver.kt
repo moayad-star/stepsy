f78be67c5d4918ea62128d21251734cc28f69163
nvllz <jasmin111@op.pl>
2025-04-24 12:56:13 +0200

    fix: remove unnecessary build check

--- app/src/main/java/com/nvllz/stepsy/service/OnBootReceiver.kt
+++ app/src/main/java/com/nvllz/stepsy/service/OnBootReceiver.kt
@@ -7,22 +7,11 @@ package com.nvllz.stepsy.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 
-/**
- * BroadcastReceiver to automatically start the [MotionService] when the device booted.
- *
- *
- * Created by tiefensuche on 10.02.18.
- */
 internal class OnBootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action?.equals(Intent.ACTION_BOOT_COMPLETED, ignoreCase = true) == true) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.startForegroundService(Intent(context, MotionService::class.java))
-            } else {
-                context.startService(Intent(context, MotionService::class.java))
-            }
+            context.startForegroundService(Intent(context, MotionService::class.java))
         }
     }
 }
